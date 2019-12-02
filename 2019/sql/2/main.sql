ae2c16358ac19930ca938d511c1878045d5cfe80
Axel Nyffenegger <Axnyff@gmail.com>
2019-12-02 20:40:41 +0100

    Exo 2 fini

--- 2019/sql/2/main.sql
+++ 2019/sql/2/main.sql
@@ -9,7 +9,7 @@ BEGIN;
 
   -- PART 1
   WITH RECURSIVE starting_data as (
-    SELECT array_cat(ARRAY[(string_to_array(value, ','))[1]::INT, 12, 2], (string_to_array(value, ',')::INT[])[4:]) as data from numbers
+    SELECT array_cat(ARRAY[(string_to_array(value, ','))[1]::BIGINT, 12, 2], (string_to_array(value, ',')::BIGINT[])[4:]) as data from numbers
   ),
   final_data(data, position) as (
     SELECT data, 1 as position from starting_data
@@ -36,6 +36,41 @@ BEGIN;
 
   SELECT data[1] as "Part 1" from final_data order by position desc limit 1;
 
-
-
   -- PART 2
+  WITH RECURSIVE nouns as (
+    SELECT * from generate_series(1, 99) as noun
+  ),
+  verbs as (
+    SELECT * from generate_series(1, 99) as verb
+  ),
+  starting_data as (
+    SELECT noun, verb, array_cat(ARRAY[(string_to_array(value, ','))[1]::BIGINT, noun, verb], (string_to_array(value, ',')::BIGINT[])[4:]) as data from numbers,
+    nouns, verbs
+  ),
+  final_data(data, noun, verb, position) as (
+    SELECT data, noun, verb, 1 as position from starting_data
+    UNION ALL
+      SELECT
+        CASE WHEN data[position] = 1
+          THEN
+          array_cat(
+            data[1:data[position + 3]],
+            array_cat(
+            ARRAY[(data[data[position + 1] + 1] + data[data[position + 2] + 1])],
+            data[data[position + 3] +2 :]))
+          WHEN data[position] = 2
+            THEN
+            array_cat(
+              data[1:data[position + 3]],
+              array_cat(
+              ARRAY[(data[data[position + 1] + 1] * data[data[position + 2] + 1])],
+              data[data[position + 3] +2:]))
+          else data
+        END
+      , noun, verb, position + 4 from final_data where data[position] != 99 AND position < array_length(data, 1)
+  )
+
+
+  SELECT noun *100 + verb as "Part 2" from final_data where data[1] = 19690720;
+
+
