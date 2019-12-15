9880ebf523b967be8e12053a987482dcbcfe7bca
Axel Nyffenegger <Axnyff@gmail.com>
2019-12-16 00:13:24 +0100

    add callbag machine

--- 2019/ts/15/main.ts
+++ 2019/ts/15/main.ts
@@ -1,129 +1,5 @@
-import { printResult } from "..";
+import { printResult, buildCallbagMachine } from "..";
 import fs from "fs";
-
-const readModes = (instruction: number, result: number[] = []): number[] => {
-  if (instruction < 10) {
-    return [...result, instruction];
-  }
-  const remainder = Math.floor(instruction / 10);
-  return readModes(remainder, [...result, instruction % 10]);
-};
-
-const getVal = (
-  target: number = 0,
-  mode: number = 0,
-  instructions: number[],
-  relativeBase: number
-) => {
-  if (mode === 1) {
-    return target;
-  } else if (mode === 0) {
-    return instructions[target] || 0;
-  }
-  return instructions[target + relativeBase] || 0;
-};
-
-const getAddress = (
-  target: number = 0,
-  mode: number = 0,
-  instructions: number[],
-  relativeBase: number
-) => {
-  if (mode === 0) {
-    return target;
-  }
-  return target + relativeBase;
-};
-
-function* readInstructions(instructions: number[]) {
-  let position = 0;
-  let input = 0;
-  let relativeBase = 0;
-  outer: while (true) {
-    const instruction = instructions[position];
-    const instructionCode = instruction % 100;
-    const modes = readModes(Math.floor(instruction / 100));
-    let first = getVal(
-      instructions[position + 1],
-      modes[0],
-      instructions,
-      relativeBase
-    );
-    let second = getVal(
-      instructions[position + 2],
-      modes[1],
-      instructions,
-      relativeBase
-    );
-    const third = getAddress(
-      instructions[position + 3],
-      modes[2],
-      instructions,
-      relativeBase
-    );
-    switch (instructionCode) {
-      case 1:
-        instructions[third] = first + second;
-        position += 4;
-        break;
-      case 2:
-        instructions[third] = first * second;
-        position += 4;
-        break;
-      case 3:
-        input = yield undefined;
-        first = getAddress(
-          instructions[position + 1],
-          modes[0],
-          instructions,
-          relativeBase
-        );
-        instructions[first] = input;
-        position += 2;
-        break;
-      case 4:
-        yield first;
-        position += 2;
-        break;
-      case 5:
-        if (first !== 0) {
-          position = second;
-        } else {
-          position += 3;
-        }
-        break;
-      case 6:
-        if (first === 0) {
-          position = second;
-        } else {
-          position += 3;
-        }
-        break;
-      case 7:
-        if (first < second) {
-          instructions[third] = 1;
-        } else {
-          instructions[third] = 0;
-        }
-        position += 4;
-        break;
-      case 8:
-        if (first === second) {
-          instructions[third] = 1;
-        } else {
-          instructions[third] = 0;
-        }
-        position += 4;
-        break;
-      case 9:
-        relativeBase += first;
-        position += 2;
-        break;
-      case 99:
-        break outer;
-    }
-  }
-}
 
 const dirs: { [K: number]: string } = {
   1: "^",
@@ -312,7 +188,7 @@ class Minimap {
     const queue: Node[] = [];
     queue.push({
       distance: 0,
-      position: this.target,
+      position: this.target
     });
 
     this.visited = new Set();
@@ -337,7 +213,7 @@ class Minimap {
         }
       }
     }
-    return maxDistance
+    return maxDistance;
   };
 }
 
@@ -347,22 +223,29 @@ const wait = (ms: number) =>
   });
 
 const solve = (data: number[]): [number, number] => {
-  const gen = readInstructions(data.slice());
+  const source = buildCallbagMachine(data);
   let output;
 
   const map = new Minimap();
   let i = 0;
-  while (i <= 6000) {
+  let talkback: any;
+  source(0, (t: number, payload?: any) => {
     i++;
-    const { value, done } = gen.next(map.direction);
-    if (done) {
-      break;
+    if (i >= 6000) {
+      talkback(2);
+      return;
     }
-    if (value !== undefined) {
-      map.addValue(value, i);
-      output = value;
+    if (t === 0) {
+      talkback = payload;
+      talkback(1);
     }
-  }
+    if (t === 1 && payload === undefined) {
+      talkback(1, map.direction);
+    }
+    if (t === 1 && payload !== undefined) {
+      map.addValue(payload, i);
+    }
+  });
   return [map.solve(), map.solvePart2()];
 };
 
--- 2019/ts/index.ts
+++ 2019/ts/index.ts
@@ -9,3 +9,145 @@ ${part2}
 
 `);
 };
+
+const readModes = (instruction: number, result: number[] = []): number[] => {
+  if (instruction < 10) {
+    return [...result, instruction];
+  }
+  const remainder = Math.floor(instruction / 10);
+  return readModes(remainder, [...result, instruction % 10]);
+};
+
+const getVal = (
+  target: number = 0,
+  mode: number = 0,
+  instructions: number[],
+  relativeBase: number
+) => {
+  if (mode === 1) {
+    return target;
+  } else if (mode === 0) {
+    return instructions[target] || 0;
+  }
+  return instructions[target + relativeBase] || 0;
+};
+
+const getAddress = (
+  target: number = 0,
+  mode: number = 0,
+  instructions: number[],
+  relativeBase: number
+) => {
+  if (mode === 0) {
+    return target;
+  }
+  return target + relativeBase;
+};
+
+export const buildCallbagMachine = (instructions: number[]) => (
+  type: number,
+  sink: any
+) => {
+  let position = 0;
+  let relativeBase = 0;
+
+  const makeSource = (input?: number) => {
+    while (true) {
+      const instruction = instructions[position];
+      const instructionCode = instruction % 100;
+      const modes = readModes(Math.floor(instruction / 100));
+      let first = getVal(
+        instructions[position + 1],
+        modes[0],
+        instructions,
+        relativeBase
+      );
+      let second = getVal(
+        instructions[position + 2],
+        modes[1],
+        instructions,
+        relativeBase
+      );
+      const third = getAddress(
+        instructions[position + 3],
+        modes[2],
+        instructions,
+        relativeBase
+      );
+      switch (instructionCode) {
+        case 1:
+          instructions[third] = first + second;
+          position += 4;
+          break;
+        case 2:
+          instructions[third] = first * second;
+          position += 4;
+          break;
+        case 3:
+          if (!input) {
+            sink(1);
+            return;
+          }
+          first = getAddress(
+            instructions[position + 1],
+            modes[0],
+            instructions,
+            relativeBase
+          );
+          instructions[first] = input;
+          input = undefined;
+          position += 2;
+          break;
+        case 4:
+          sink(1, first);
+          position += 2;
+          break;
+        case 5:
+          if (first !== 0) {
+            position = second;
+          } else {
+            position += 3;
+          }
+          break;
+        case 6:
+          if (first === 0) {
+            position = second;
+          } else {
+            position += 3;
+          }
+          break;
+        case 7:
+          if (first < second) {
+            instructions[third] = 1;
+          } else {
+            instructions[third] = 0;
+          }
+          position += 4;
+          break;
+        case 8:
+          if (first === second) {
+            instructions[third] = 1;
+          } else {
+            instructions[third] = 0;
+          }
+          position += 4;
+          break;
+        case 9:
+          relativeBase += first;
+          position += 2;
+          break;
+        case 99:
+          sink(2);
+          return;
+      }
+    }
+  };
+
+  if (type === 0) {
+    sink(0, (t: number, payload?: number) => {
+      if (t === 1) {
+        makeSource(payload);
+      }
+    });
+  }
+};
