0d8f1b003cbde81edaf59a8c1b07d9d9c32adc23
Axel Nyffenegger <Axnyff@gmail.com>
2021-12-17 10:25:31 +0100

    Wip

--- 2021/17/part2.rb
+++ 2021/17/part2.rb
@@ -1,4 +1,4 @@
-file = File.open("test-input")
+file = File.open("input")
 file_data = file.readlines.map(&:chomp)[0]
 file.close
 file_data = file_data.gsub("target area: ", "")
@@ -15,34 +15,34 @@ $max = 0
 def valid(xv, yv)
   x = 0
   y = 0
+  init_xv = xv
+  init_yv = yv
   while x < $x1 
     x = x + xv
     y = y + yv
     if xv > 0
       xv = xv - 1
-    else
-      xv = xv + 1
     end
     yv = yv -1
     if x >= $x0 and x <= $x1 && y>= $y0 && y <= $y1
       return true
     end
-    if x == 0
-      return false
+    if xv == 0
+      if y < $y0 and yv < 0
+        return false
+      end
     end
   end
   return false
 end
 
-ymax = 0
-
-y = -50
+YMIN = -5000
+y = YMIN
 x = 0
-
 
 count = 0
 while x < 1000
-  y = -50
+  y = YMIN
   while y < 1000
     if valid(x, y)
       count = count + 1
