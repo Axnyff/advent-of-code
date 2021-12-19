281035045713072f960c8802e35ac747ec3ef0f7
Axel Nyffenegger <Axnyff@gmail.com>
2021-12-19 17:40:22 +0100

    Wip

--- 2021/19/part1.scala
+++ 2021/19/part1.scala
@@ -1,50 +1,46 @@
 import scala.io.Source
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.Map
 
 object Part1 {
   def rotations(point: (Int, Int, Int)): Seq[(Int, Int, Int)] = {
     val (x, y, z) = point
     return Seq(
-      (x, y, z),
-      (-x, -y, z),
-      (-y, x, z),
-      (y, -x, z),
-
-      (-z, y, -x),
-      (z, -y, -x),
-      (-y, -z, -x),
-      (y, z, -x),
-
-      (-x, y, -z),
-      (x, -y, -z),
-      (-y, -x, -z),
-      (y, x, -z),
-
-      (z, y, -x),
-      (-z, -y, -x),
-      (-y, z, -x),
-      (y, -z, -x),
-
-      (x, z, -y),
-      (-x, -z, -y),
-      (-z, x, -y),
-      (z, -x, -y),
-
-      (x, -z , y),
-      (-x, z , y),
-      (z, x , y),
-      (-z, -x , y),
-    )
+      ( x, y, z ),
+      ( y, z, x ),
+      ( z, x, y ),
+      ( -x, z, y ),
+      ( z, y, -x ),
+      ( y, -x, z ),
+      ( x, z, -y ),
+      ( z, -y, x ),
+      ( -y, x, z ),
+      ( x, -z, y ),
+      ( -z, y, x ),
+      ( y, x, -z ),
+      ( -x, -y, z ),
+      ( -y, z, -x ),
+      ( z, -x, -y ),
+      ( -x, y, -z ),
+      ( y, -z, -x ),
+      ( -z, -x, y ),
+      ( x, -y, -z ),
+      ( -y, -z, x ),
+      ( -z, x, -y ),
+      ( -x, -z, -y ),
+      ( -z, -y, -x ),
+      ( -y, -x, -z ),
+      )
   }
 
   def applyTransFormation(item: (Int, Int, Int), transformation: ((Int, Int, Int), Int)): (Int, Int, Int) = {
-    val (rot, index) = transformation
-    val (x, y, z) = rotations(rot)(index)
+    val ((x, y, z), index) = transformation
     val (x0, y0, z0) = rotations(item)(index)
     (x0 + x, y0 + y, z + z0)
   }
 
   def main(args: Array[String]) = {
-    val fileContents = Source.fromFile("test-input").getLines.mkString("\n")
+    val fileContents = Source.fromFile("input").getLines.mkString("\n")
 
     var scanners: Seq[Seq[(Int, Int, Int)]] = fileContents.split("\n\n").map { s =>
       val items = s.split("\n").drop(1)
@@ -54,51 +50,74 @@ object Part1 {
       }).toSeq
     }.toSeq
 
-    var correctScanners = scanners.take(1)
-    scanners = scanners.drop(1)
 
     var i = 0
     var count = 0
 
-    while (scanners.nonEmpty) {
-      for (i <- scanners.length - 1 to 0 by -1) {
+    var map = scala.collection.mutable.Map[Int, Map[Int, ((Int, Int, Int), Int)]]()
 
-        val transformation = correctScanners.flatMap { scanner =>
-          val items = scanner.flatMap { item1 =>
-            scanners(i).flatMap { item0 => 
-              val (x0, y0, z0) = item0
+    for (i <- 0 to scanners.length - 1) {
+      for (j <- 0 to scanners.length -1) {
+        if (i == j) {
+        } else {
+          val scanner = scanners(j)
+          val scanner2 = scanners(j)
+
+          val items = scanner.flatMap { item0 =>
+            scanners(i).flatMap { item1 => 
+              val (x, y, z) = item0
               rotations(item1).zipWithIndex.map {
-                case ((x, y, z), i) => {
-                  if (x-x0 == -68) {
-                  }
+                case ((x0, y0, z0), i) => {
                   (x - x0, y - y0, z - z0, i)
                 }
               }
             }
           }
-          (items.groupBy(identity).mapValues(_.size).toSeq.filter(_._2 >= 12)).headOption.map {
-            case ((x, y, z, index), count) => (((x, y, z), index), scanner)
+          val res = (items.groupBy(identity).mapValues(_.size).toSeq.filter(_._2 >= 12)).headOption.map {
+            case ((x, y, z, index), count) => (((x, y, z), index))
           }
-        }.headOption
-        if (transformation.isDefined) {
-          val allItems = transformation.get._2
-          val newScanner = scanners(i).map(item => applyTransFormation(item, transformation.get._1)).sorted
-
-          val newScannerTemp = (0 to 23).map(i => newScanner.map(item => rotations(item)(i))).sortBy(items =>
-              items.filter(allItems.contains(_)).length).last.sorted
-          val correctCount = newScannerTemp.filter(allItems.contains(_)).length
-          if (correctCount != 0) {
-            correctScanners = correctScanners :+ newScannerTemp
-            scanners = scanners.take(i) ++ scanners.drop(i + 1)
-            count += 1
+          if (res.isDefined) {
+            if (i == 21) {
+              println(i)
+            }
+            val key = (i, j)
+            val index = res.get._2
+            var nested = map.getOrElse(i, scala.collection.mutable.Map[Int, ((Int, Int, Int), Int)](j -> (res.get._1, index)))
+            nested = nested + (j -> (res.get._1, index))
+            map = map + (i -> nested)
           }
-
         }
       }
     }
-    val allItems = correctScanners.flatten.distinct.sorted.toSeq
-    println(allItems.length)
+    var allItems = scanners(0)
 
+    def getPath(currentPath: Seq[Int]): Seq[Int] = {
+      if (map.get(currentPath.last).exists(_.contains(0))) {
+        return currentPath :+ 0
+      }
+      for (item <- map.get(currentPath.last).map(_.keys).getOrElse(Seq.empty)) {
+        if (!currentPath.contains(item)) {
+          val newPath = getPath(currentPath :+ item)
+          if (newPath.nonEmpty) {
+            return newPath
+          }
+        }
+      }
+      return Seq.empty
+    }
 
+    for (i <- 1 to scanners.length - 1) {
+      val path = getPath(Seq(i))
+      var pos = i
+      var newItems = scanners(i)
+      for (segment <- path.drop(1)) {
+        val target = map.get(pos).get.get(segment).get
+        pos = segment
+        newItems = newItems.map(item => applyTransFormation(item, target))
+      }
+      allItems = allItems ++ newItems
+    }
+
+    println(allItems.distinct.length)
   }
 }
