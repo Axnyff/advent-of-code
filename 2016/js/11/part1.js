b10b2323f70611b44ee54a0a97bd631da4b0e39f
Axel Nyffenegger <Axnyff@gmail.com>
2021-12-20 15:33:52 +0100

    Wip

--- 2016/js/11/part1.js
+++ 2016/js/11/part1.js
@@ -1,31 +1,46 @@
 "use strict";
-const testing = true;
+const testing = false;
 
 const floors = testing
   ? [["HM", "LM"], ["HG"], ["LG"], []]
   : [
-      ["SG", "SM", "PG", "PM"].sort(),
-      ["TG", "RG", "RM", "CG", "CM"].sort(),
+      ["SG", "SM", "PG", "PM"],
+      ["TG", "RG", "RM", "CG", "CM"],
       ["TM"],
       [],
     ];
 
-const isSolved = (state) => {
-  for (let i = 0; i < state.length - 1; i++) {
-    if (state[i].length !== 0) {
-      return false;
-    }
-  }
-  return true;
+const dp = {};
+const stringifyState = (state, elevator) => {
+  const copied = state.map(el => el.slice().sort());
+  return JSON.stringify([copied, elevator]);
 };
+
+const explored = {};
+const finalState = testing ? 
+  stringifyState([
+  [],
+  [],
+  [],
+  ["HM", "LM", "HG", "LG"],
+], 3) :
+
+  stringifyState([
+  [],
+  [],
+  [],
+  ["SG", "SM", "PG", "PM", "TG", "RG", "RM", "CG", "CM", "TM"],
+], 3);
+
+dp[stringifyState(floors, 0)] = 0;
 
 const isDead = (state) => {
   for (let i = 0; i < state.length; i++) {
     const floor = state[i];
     for (let item of floor) {
       if (item.endsWith("M")) {
+
         if (
-          floor.some((element) => element.endsWith("M")) &&
           floor.some((element) => element.endsWith("G")) &&
           !floor.some((element) => element == item.replace("M", "G"))
         ) {
@@ -33,106 +48,81 @@ const isDead = (state) => {
         }
       }
     }
-    if (state[i].length !== 0) {
-      return false;
-    }
   }
   return false;
 };
-let elevator = 0;
 
-const solutions = {};
-const seen = {};
-
-const printState = (state, elevator, context = "", print = true) => {
-  let real_res = "";
-  for (let i = state.length - 1; i >= 0; i--) {
-    const floor = state[i];
-    let res = "F" + (i + 1);
-    if (i === elevator) {
-      res += " E";
-    } else {
-      res += " .";
-    }
-    for (let item of floor) {
-      res += " " + item + " ";
-    }
-    real_res = real_res + res + "\n";
-  }
-  if (print) {
-    console.log(context + ": " + "\n" + real_res);
-  }
-};
-const termination = 5;
-
-const solve = (state, elevator = 0, steps = 0) => {
-  if (steps > 12) {
-    return Infinity;
-  }
-  seen[JSON.stringify([state, elevator])] = true;
-  if (isSolved(state)) {
-    console.log("WEH");
-    printState(state, elevator, "SOLVED");
-    solutions[JSON.stringify(state)] = steps;
-    return steps;
-  }
-  if (isDead(state)) {
-    printState(state, elevator, "OH NO DEAD " + steps, steps < 3);
-    return Infinity;
-  }
-  printState(state, elevator, "STEP " + steps, steps < 3);
-  const possibles = [];
-  const floor = state[elevator];
-
-  for (let i of floor) {
-    for (let j of floor) {
-      if (i === j) {
-        possibles.push([i]);
-      } else {
-        if (!possibles.some(([i0, j0]) => (i0 === i && j0 === j) || (i0 === j && j0 === i))) {
-          possibles.push([i, j]);
-        }
-      }
-    }
-  }
-  if (steps < 3) {
-    console.log(possibles, steps);
-  }
-  const potentials = [];
-  for (let i of [1, -1]) {
-    const newElevator = elevator + i;
-    if (state[newElevator] === undefined) {
+let iter = 0;
+while (!dp[finalState]) {
+  iter++;
+  for (let key of Object.keys(dp)) {
+    if (explored[key]) {
       continue;
+    } else {
+      explored[key];
     }
-    for (let possible of possibles) {
-      const newState = state.map((floor, index) => {
-        if (index === elevator) {
-          return floor.filter((el) => !possible.includes(el));
+    const [state, elevator] = JSON.parse(key);
+    const itemsAtFloor = state[elevator];
+    const raw_possible = new Set();
+    for (let i of itemsAtFloor) {
+      for (let j of itemsAtFloor) {
+        if (i !== j) {
+          let item = [i, j].sort();
+          raw_possible.add(item.join("-"));
+        } else {
+          raw_possible.add(i);
         }
-        if (index === newElevator) {
-          const newFloor = [...floor, ...possible];
-          newFloor.sort();
-          return newFloor;
-        }
-        return floor;
-      });
-      printState(newState, newElevator, "Pretendant for step " + (steps + 1), steps < 3);
-      if (!seen[JSON.stringify([newState, newElevator])]) {
-        console.log("go on");
-        potentials.push(solve(newState, newElevator, steps + 1));
-      } else {
-        console.log("SEEN THAT shit");
       }
     }
+    const possibles = [...raw_possible].map(el => el.split("-"));
+
+    const down = elevator - 1;
+    if (down >= 0) {
+      possibles.forEach(possible => {
+        const newState = state.map((floor, floorIndex) => {
+          if (floorIndex === elevator) {
+            return floor.filter(el => !possible.includes(el));
+          }
+          if (floorIndex === down) {
+            return [...floor, ...possible];
+          }
+          return floor;
+        });
+        if (!isDead(newState)) {
+          const str = stringifyState(newState, down);
+
+          if (dp[str] === undefined || dp[str] > dp[key] + 1) {
+            dp[stringifyState(newState, down)] = dp[key] + 1;
+          }
+        }
+      })
+
+    }
+    const up = elevator + 1;
+    if (up < state.length) {
+      possibles.forEach(possible => {
+        const newState = state.map((floor, floorIndex) => {
+          if (floorIndex === elevator) {
+            return floor.filter(el => !possible.includes(el));
+          }
+          if (floorIndex === up) {
+            return [...floor, ...possible];
+          }
+          return floor;
+        });
+        if (!isDead(newState)) {
+          const str = stringifyState(newState, up);
+
+          if (dp[str] === undefined || dp[str] > dp[key] + 1) {
+            dp[stringifyState(newState, up)] = dp[key] + 1;
+          }
+        } else {
+
+        }
+      })
+    }
   }
-  return Math.min(...potentials);
-};
+  console.log(iter);
+}
 
-// console.log(isDead([
-//   ["LM"],
-//   [],
-//   ["HG", "HM", "LG"],
-//   [],
-// ]))
-
-console.log(solve(floors));
+console.log(dp[finalState]);
