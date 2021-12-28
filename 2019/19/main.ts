31779858a90936d3be46ead23157f26b8472b72a
Axel Nyffenegger <Axnyff@gmail.com>
2021-12-28 13:17:12 +0100

    Wip

--- 2019/19/main.ts
+++ 2019/19/main.ts
@@ -1,7 +1,6 @@
-import { printResult, buildCallbagMachine } from "..";
-import fs from "fs";
+const fs = require("fs");
 
-const readModes = (instruction: number, result: number[] = []): number[] => {
+const readModes = (instruction, result = []) => {
   if (instruction < 10) {
     return [...result, instruction];
   }
@@ -9,12 +8,7 @@ const readModes = (instruction: number, result: number[] = []): number[] => {
   return readModes(remainder, [...result, instruction % 10]);
 };
 
-const getVal = (
-  target: number = 0,
-  mode: number = 0,
-  instructions: number[],
-  relativeBase: number
-) => {
+const getVal = (target = 0, mode = 0, instructions, relativeBase) => {
   if (mode === 1) {
     return target;
   } else if (mode === 0) {
@@ -23,134 +17,165 @@ const getVal = (
   return instructions[target + relativeBase] || 0;
 };
 
-const getAddress = (
-  target: number = 0,
-  mode: number = 0,
-  instructions: number[],
-  relativeBase: number
-) => {
+const getAddress = (target = 0, mode = 0, instructions, relativeBase) => {
   if (mode === 0) {
     return target;
   }
   return target + relativeBase;
 };
 
-function* readInstructions(instructions: number[]) {
-  let position = 0;
-  let input = 0;
-  let relativeBase = 0;
-  outer: while (true) {
-    const instruction = instructions[position];
-    const instructionCode = instruction % 100;
-    const modes = readModes(Math.floor(instruction / 100));
-    let first = getVal(
-      instructions[position + 1],
-      modes[0],
-      instructions,
-      relativeBase
-    );
-    let second = getVal(
-      instructions[position + 2],
-      modes[1],
-      instructions,
-      relativeBase
-    );
-    const third = getAddress(
-      instructions[position + 3],
-      modes[2],
-      instructions,
-      relativeBase
-    );
-    switch (instructionCode) {
-      case 1:
-        instructions[third] = first + second;
-        position += 4;
-        break;
-      case 2:
-        instructions[third] = first * second;
-        position += 4;
-        break;
-      case 3:
-        input = yield undefined;
-        first = getAddress(
-          instructions[position + 1],
-          modes[0],
-          instructions,
-          relativeBase
-        );
-        instructions[first] = input;
-        position += 2;
-        break;
-      case 4:
-        yield first;
-        position += 2;
-        break;
-      case 5:
-        if (first !== 0) {
-          position = second;
-        } else {
-          position += 3;
-        }
-        break;
-      case 6:
-        if (first === 0) {
-          position = second;
-        } else {
-          position += 3;
-        }
-        break;
-      case 7:
-        if (first < second) {
-          instructions[third] = 1;
-        } else {
-          instructions[third] = 0;
-        }
-        position += 4;
-        break;
-      case 8:
-        if (first === second) {
-          instructions[third] = 1;
-        } else {
-          instructions[third] = 0;
-        }
-        position += 4;
-        break;
-      case 9:
-        relativeBase += first;
-        position += 2;
-        break;
-      case 99:
-        break outer;
-    }
-  }
-}
-const part1 = (data: number[]) => {
-
-  let output = 0;
-  for (let x = 0; x < 50; x++) {
-    for (let y = 0; y < 50; y++) {
-      const gen = readInstructions(data.slice());
-      gen.next(x);
-      gen.next(y);
-      while(true) {
-        const { value, done } = gen.next();
-        console.log(x, y, value);
-        output += (value || 0);
-        if (done) {
-          break;
-        }
+const readInstructions = (
+  instructions,
+  [position, relativeBase],
+  input,
+  output
+) => {
+  const instruction = instructions[position];
+  const instructionCode = instruction % 100;
+  const modes = readModes(Math.floor(instruction / 100));
+  let first = getVal(
+    instructions[position + 1],
+    modes[0],
+    instructions,
+    relativeBase
+  );
+  let second = getVal(
+    instructions[position + 2],
+    modes[1],
+    instructions,
+    relativeBase
+  );
+  const third = getAddress(
+    instructions[position + 3],
+    modes[2],
+    instructions,
+    relativeBase
+  );
+  switch (instructionCode) {
+    case 1:
+      instructions[third] = first + second;
+      position += 4;
+      break;
+    case 2:
+      instructions[third] = first * second;
+      position += 4;
+      break;
+    case 3:
+      first = getAddress(
+        instructions[position + 1],
+        modes[0],
+        instructions,
+        relativeBase
+      );
+      instructions[first] = input.shift();
+      position += 2;
+      break;
+    case 4:
+      output.push(first);
+      position += 2;
+      break;
+    case 5:
+      if (first !== 0) {
+        position = second;
+      } else {
+        position += 3;
       }
-    }
+      break;
+    case 6:
+      if (first === 0) {
+        position = second;
+      } else {
+        position += 3;
+      }
+      break;
+    case 7:
+      if (first < second) {
+        instructions[third] = 1;
+      } else {
+        instructions[third] = 0;
+      }
+      position += 4;
+      break;
+    case 8:
+      if (first === second) {
+        instructions[third] = 1;
+      } else {
+        instructions[third] = 0;
+      }
+      position += 4;
+      break;
+    case 9:
+      relativeBase += first;
+      position += 2;
+      break;
+    case 99:
+      return null;
   }
-  console.log(output);
+  return [position, relativeBase];
 };
 
-fs.readFile(`${__dirname}/input`, (err: Error | null, rawData: Buffer) => {
-  const data = rawData
-    .toString()
-    .split(",")
-    .filter(el => el !== "")
-    .map(el => parseInt(el, 10));
 
-  part1(data.slice());
-});
+const cache = {};
+const executeIns = (x, y) => {
+  const key = `${x},${y}`;
+  if (cache[key]) {
+    return cache[key];
+  }
+  const instructions = data.slice();
+  const input = [x, y];
+  const output = [];
+  let position = 0;
+  let relativeBase = 0;
+  while (true) {
+    const res = readInstructions(
+      instructions,
+      [position, relativeBase],
+      input,
+      output
+    );
+    if (res === null) {
+      break;
+    }
+    [position, relativeBase] = res;
+  }
+  cache[key] = output[0];
+  return output[0];
+};
+
+
+
+const offset = 99;
+let total = 0;
+const part1 = (data) => {
+  let y = 10;
+  let x = 0;
+  while(true) {
+    console.log(x, y, executeIns(x, y));
+    while (executeIns(x, y) === 0) {
+      x++;
+    }
+    while (executeIns(x, y) === 1) {
+      x++;
+    }
+    x--;
+    if (executeIns(x, y + offset) === 0) {
+      y++;
+      continue;
+    }
+    if (executeIns(x - offset, y + offset) === 0) {
+      y++;
+      continue;
+    }
+    break;
+  }
+  console.log((x - offset) * 10000 + y);
+};
+
+const data = fs
+  .readFileSync("input")
+  .toString()
+  .trimEnd()
+  .split(",")
+  .map(Number);
+
+part1(data.slice());
