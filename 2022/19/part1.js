e8500d3f48b4e164ac1feefdf181a6a26b596558
Axel Nyffenegger <Axnyff@gmail.com>
2023-11-05 02:03:27 +0100

    Wip

--- 2022/19/part1.js
+++ 2022/19/part1.js
@@ -1,10 +1,17 @@
-const blueprint = {
-  ore: 4,
-  clay: 2,
-  obsidian: [3, 14],
-  geode: [2, 7],
-};
-
+const blueprints = [
+  {
+    ore: 4,
+    clay: 2,
+    obsidian: [3, 14],
+    geode: [2, 7],
+  },
+  {
+    ore: 2,
+    clay: 3,
+    obsidian: [3, 8],
+    geode: [3, 12],
+  },
+];
 
 const computeState = (blueprint, events) => {
   let state = {
@@ -24,13 +31,13 @@ const computeState = (blueprint, events) => {
     state.obsidian += state.obsidianBot;
     state.geode += state.geodeBot;
     if (event === "G") {
-      state.ore -= blueprint.geode[0]
-      state.obsidian -= blueprint.geode[1]
+      state.ore -= blueprint.geode[0];
+      state.obsidian -= blueprint.geode[1];
       state.geodeBot++;
     }
     if (event === "B") {
-      state.ore -= blueprint.obsidian[0]
-      state.clay -= blueprint.obsidian[1]
+      state.ore -= blueprint.obsidian[0];
+      state.clay -= blueprint.obsidian[1];
       state.obsidianBot++;
     }
     if (event === "O") {
@@ -49,22 +56,14 @@ const computeState = (blueprint, events) => {
 const getMaxOutputForBluePrint = (blueprint) => {
   let toExplore = new Set();
   toExplore.add("");
-  let time = 0;
   let maxGeode = 0;
-  while (time <= 24) {
-    time++;
+  while (toExplore.size) {
     let newToExplore = new Set();
     for (let events of toExplore) {
       const state = computeState(blueprint, events);
-      if (state.time >= 24) {
-        if (state.geode === 11) {
-          console.log(state);
-          console.log(maxGeode, events);
-          return;
-        }
+      if (state.time >= 23) {
         if (state.geode > maxGeode) {
           maxGeode = Math.max(maxGeode, state.geode);
-          console.log("MAX", maxGeode);
         }
         continue;
       }
@@ -73,19 +72,16 @@ const getMaxOutputForBluePrint = (blueprint) => {
       // buy ore
       const oreMissing = blueprint.ore - state.ore;
       const oreTime = Math.ceil(oreMissing / state.oreBot);
-      if (events.length + oreTime <= 24) {
+      if (events.length + oreTime <= 23) {
         add = true;
         newToExplore.add(events + "".padEnd(oreTime, "W") + "O");
       }
 
       // buy clay
       const oreMissing2 = blueprint.clay - state.ore;
-      if (events === "WWC") {
-        console.log(oreMissing2, blueprint.clay, state.ore);
-      }
       const clayTime = Math.ceil(oreMissing2 / state.oreBot);
-      if (events.length + clayTime <= 24) {
-          add = true;
+      if (events.length + clayTime <= 23) {
+        add = true;
         newToExplore.add(events + "".padEnd(clayTime, "W") + "C");
       }
 
@@ -94,8 +90,11 @@ const getMaxOutputForBluePrint = (blueprint) => {
         const oreMissing = blueprint.obsidian[0] - state.ore;
         const clayMissing = blueprint.obsidian[1] - state.clay;
 
-        const obsidianTime = Math.max(Math.ceil(clayMissing / state.clayBot), Math.ceil(oreMissing / state.oreBot));
-        if (events.length + obsidianTime <= 24) {
+        const obsidianTime = Math.max(
+          Math.ceil(clayMissing / state.clayBot),
+          Math.ceil(oreMissing / state.oreBot)
+        );
+        if (events.length + obsidianTime <= 23) {
           add = true;
           newToExplore.add(events + "".padEnd(obsidianTime, "W") + "B");
         }
@@ -106,8 +105,11 @@ const getMaxOutputForBluePrint = (blueprint) => {
         const oreMissing = blueprint.geode[0] - state.ore;
         const obsidianMissing = blueprint.geode[1] - state.obsidian;
 
-        const geodeTime = Math.max(Math.ceil(obsidianMissing / state.obsidianBot), Math.ceil(oreMissing / state.oreBot));
-        if (events.length + geodeTime <= 24) {
+        const geodeTime = Math.max(
+          Math.ceil(obsidianMissing / state.obsidianBot),
+          Math.ceil(oreMissing / state.oreBot)
+        );
+        if (events.length + geodeTime <= 23) {
           add = true;
           newToExplore.add(events + "".padEnd(geodeTime, "W") + "G");
         }
@@ -121,6 +123,11 @@ const getMaxOutputForBluePrint = (blueprint) => {
   return maxGeode;
 };
 
-
-// console.log(computeState(blueprint, "WWCWCWCWWWBCWWBWWGWWGWWWG"));
-console.log(getMaxOutputForBluePrint(blueprint));
+let total = 0;
+for (let i = 0; i < blueprints.length; i++) {
+  console.log(i);
+  const blueprint = blueprints[i];
+  const max = getMaxOutputForBluePrint(blueprint);
+  total += (i + 1) * max;
+}
+console.log(total);
