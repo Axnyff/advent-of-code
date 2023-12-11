559c59afbf164f81221cfac6820eb32ccd036611
Axel Nyffenegger <Axnyff@gmail.com>
2023-12-11 11:09:26 +0100

    Wip

---
+++ 2023/11/main.js
@@ -0,0 +1,63 @@
+const lines = require("fs").readFileSync("input").toString().slice(0, -1).split("\n");
+const stars = [];
+lines.forEach((l, y) => {
+  l.split("").forEach((c, x) => {
+    if (c === "#") {
+      stars.push([x, y]);
+    }
+  });
+});
+
+const rows = stars.map(el => el[0]);
+const cols = stars.map(el => el[1]);
+
+let total = 0;
+for (let i = 0; i < stars.length; i++) {
+  for (let j = i + 1; j < stars.length; j++) {
+    const [x, y] = stars[i];
+    const [x2, y2] = stars[j];
+    let subtotal = 0;
+    for (let k = Math.min(x, x2); k < Math.max(x, x2); k++) {
+      if (rows.includes(k)) {
+        subtotal += 1;
+      } else {
+        subtotal += 2;
+      }
+    }
+    for (let k = Math.min(y, y2); k < Math.max(y, y2); k++) {
+      if (cols.includes(k)) {
+        subtotal += 1;
+      } else {
+        subtotal += 2;
+      }
+    }
+    total += subtotal;
+  }
+}
+console.log(total);
+
+let total2 = 0;
+const ratio = 1_000_000;
+for (let i = 0; i < stars.length; i++) {
+  for (let j = i + 1; j < stars.length; j++) {
+    const [x, y] = stars[i];
+    const [x2, y2] = stars[j];
+    let subtotal = 0;
+    for (let k = Math.min(x, x2); k < Math.max(x, x2); k++) {
+      if (rows.includes(k)) {
+        subtotal += 1;
+      } else {
+        subtotal += ratio;
+      }
+    }
+    for (let k = Math.min(y, y2); k < Math.max(y, y2); k++) {
+      if (cols.includes(k)) {
+        subtotal += 1;
+      } else {
+        subtotal += ratio;
+      }
+    }
+    total2 += subtotal;
+  }
+}
+console.log(total2);
