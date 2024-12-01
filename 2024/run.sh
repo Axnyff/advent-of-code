ce5aca81b41101600d96500e55bcc5b3eba4c721
Axel Nyffenegger <Axnyff@gmail.com>
2024-12-01 18:16:13 +0100

    Wip

--- 2024/run.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -eq 0 ]
-  then
-    psql -d hemea -h localhost -U hemea
-  else
-    psql -d hemea -h localhost -U hemea -f $1
-fi
--- 2024/start_db.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 5434:5434 --name advent --restart=always -e POSTGRES_USER=advent -e POSTGRES_DB=advent -e POSTGRES_PASSWORD=advent -d postgres
