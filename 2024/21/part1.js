d78ad937187b436cc15c9737c59de42348f31ea4
Axel Nyffenegger <Axnyff@gmail.com>
2025-06-05 14:35:09 +0200

    Wip

--- 2024/21/part1.js
+++ 2024/21/part1.js
@@ -100,28 +100,159 @@ const computeScore = (codes) => {
 const tokey = (x, y) => `${x}-${y}`;
 const unkey = k => k.split("-").map(Number)
 
+const getPath = (x, y) => {
+  if (x === 1) {
+    return ">"
+  }
+  if (x === -1) {
+    return "<"
+  }
+  if (y === -1) {
+    return "^"
+  }
+  return "v";
+};
+
 const computePath = (start, end) => {
   let explored = new Set();
   let toExplore = new Map();
   let result = [];
-  toExplore.set(start, "");
+  toExplore.set("", start);
   while (toExplore.size) {
-    let newToExplore = new Set();
+    let newToExplore = new Map();
     for (let [path, pos] of toExplore) {
-      console.log(Object.entries(keypad));
       let [x, y] = Object.entries(keypad).find(e => e[0] === pos)[1];
-      console.log(x, y);
-      return;
+      for (let [diffx, diffy] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
+        let newx = diffx + x;
+        let newy = diffy + y;
+
+        const newPos = Object.entries(keypad).find(e => e[1][0] === newx && e[1][1] === newy);
+        if (newPos) {
+          const newPath = path + getPath(diffx, diffy);
+          if (newPos[0] === end) {
+            result.push(newPath);
+          } else {
+            newToExplore.set(newPath, newPos[0]);
+          }
+        }
+
+      }
     }
     toExplore = newToExplore;
     if (result.length) {
       return result;
     }
   }
-
 };
-// console.log(computeScore(codes));
-// console.log(typeFirstCode(codes.at(-1)));
-// console.log(typeSecondCode(typeFirstCode(codes.at(-1))));
-// console.log(typeSecondCode(typeSecondCode(typeFirstCode(codes.at(-1)))));
-computePath("A", "1");
+
+const computePath2 = (start, end) => {
+  let explored = new Set();
+  let toExplore = new Map();
+  let result = [];
+  toExplore.set("", start);
+  while (toExplore.size) {
+    let newToExplore = new Map();
+    for (let [path, pos] of toExplore) {
+      let [x, y] = Object.entries(keypad2).find(e => e[0] === pos)[1];
+      for (let [diffx, diffy] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
+        let newx = diffx + x;
+        let newy = diffy + y;
+
+        const newPos = Object.entries(keypad2).find(e => e[1][0] === newx && e[1][1] === newy);
+        if (newPos) {
+          const newPath = path + getPath(diffx, diffy);
+          if (newPos[0] === end) {
+            result.push(newPath);
+          } else {
+            newToExplore.set(newPath, newPos[0]);
+          }
+        }
+
+      }
+    }
+    toExplore = newToExplore;
+    if (result.length) {
+      return result;
+    }
+  }
+};
+
+const paths = {};
+for (let start of Object.keys(keypad)) {
+  for (let end of Object.keys(keypad)) {
+    if (start !== end) {
+      paths[start] = paths[start] || {}
+      paths[start][end] = computePath(start, end);
+      computePath
+    }
+  }
+}
+
+const paths2 = {};
+for (let start of Object.keys(keypad2)) {
+  for (let end of Object.keys(keypad2)) {
+    if (start !== end) {
+      paths2[start] = paths2[start] || {}
+      paths2[start][end] = computePath2(start, end);
+      computePath
+    } else {
+      paths2[start] = paths2[start] || {}
+      paths2[start][end] = [""]
+    }
+  }
+}
+
+const getFirstCodes = (code) => {
+  const trueCode = "A" + code;
+  let possibles = [""];
+  for (let i = 0; i < trueCode.length - 1; i++) {
+    let res = paths[trueCode[i]][trueCode[i+1]]
+    let newPossibles = [];
+    for (let possible of possibles) {
+      for (let path of res) {
+        newPossibles.push(possible + path);
+      }
+    }
+    possibles = newPossibles;
+  }
+  return possibles;
+};
+
+const getSecondCodes = (code) => {
+  const trueCode = "A" + code;
+  let possibles = [""];
+  for (let i = 0; i < trueCode.length - 1; i++) {
+    let res = paths2[trueCode[i]][trueCode[i+1]]
+    let newPossibles = [];
+    for (let possible of possibles) {
+      for (let path of res) {
+        newPossibles.push(possible + path + "A");
+      }
+    }
+    possibles = newPossibles;
+  }
+  return possibles;
+};
+
+const getShortest = (code) => {
+  const start = getFirstCodes(code);
+  console.log(start);
+  const result = [];
+  for (let path of start) {
+    console.log(path);
+    result.push(...getSecondCodes(path));
+  }
+
+  console.log(result);
+  const result2 = [];
+  for (let path of result) {
+    result2.push(...getSecondCodes(path));
+  }
+  const result3 = [];
+  for (let path of result2) {
+    result3.push(...getSecondCodes(path));
+  }
+  return Math.min(...result3.map(e => e.length))
+};
+
+console.log(getShortest(codes.at(-1)));
